bcfb6d4fef25d6786c6454b2eb31fb1cbd1a593a
kimsihyun <rlatlgus0129@naver.com>
2024-07-31 11:39:43 +0900

    목록 뷰페이저 연동

--- app/src/main/java/com/example/kesi/ListFragment.kt
+++ app/src/main/java/com/example/kesi/ListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.kesi
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.kesi.databinding.FragmentListBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class ListFragment : Fragment() {
     lateinit var binding: FragmentListBinding
@@ -22,12 +25,29 @@ class ListFragment : Fragment() {
         override fun getItemCount(): Int = fragments.size
         override fun createFragment(position: Int): Fragment = fragments[position]
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentListBinding.inflate(layoutInflater)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentListBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //뷰 페이저에 어댑터 적용
+        val adapter = ListFragment.FragmentPagerAdapter(requireActivity())
+        binding.viewPager.adapter = adapter
+        //탭과 뷰 페이저 연동
+        TabLayoutMediator(binding.tab, binding.viewPager) {
+                tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "그룹"
+                }
+                1 -> {
+                    tab.text = "친구"
+                }
+            }
+        }.attach()
     }
 }
