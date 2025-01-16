ce274b56c9f46cd9a655e0965051c270ffe0b4c2
kimsihyun <rlatlgus0129@naver.com>
2025-01-16 18:09:15 +0900

    chatActivity 수정 중

--- app/src/main/java/com/example/kesi/activity/ChatActivity.kt
+++ app/src/main/java/com/example/kesi/activity/ChatActivity.kt
@@ -38,10 +38,9 @@ class ChatActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         gid = intent.getLongExtra("gid",0);
-        Toast.makeText(this,gid.toString(),Toast.LENGTH_SHORT).show()
         groupApi.getGroup(gid!!).enqueue(object : Callback<GroupDto> {
             override fun onResponse(p0: Call<GroupDto>, response: Response<GroupDto>) {
-                Toast.makeText(this@ChatActivity,response.code(),Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@ChatActivity,"ad",Toast.LENGTH_SHORT).show()
                 // 액션바에 그룹 이름 보여주기
                 supportActionBar?.title = response.body()?.groupName;
             }
@@ -49,7 +48,6 @@ class ChatActivity : AppCompatActivity() {
             override fun onFailure(p0: Call<GroupDto>, p1: Throwable) {
                 Toast.makeText(this@ChatActivity,p1.message,Toast.LENGTH_SHORT).show()
             }
-
         })
 
         /*// 메시지 리스트 초기화
@@ -66,42 +64,7 @@ class ChatActivity : AppCompatActivity() {
         // DB 초기화
         database = Firebase.database.reference*/
 
-        /*// 그룹 ID 가져오기
-        database.child("groups").get().addOnSuccessListener { snapshot ->
-            if (snapshot.exists()) {
-                for (groupSnapshot in snapshot.children) {
-                    val groupName = groupSnapshot.child("groupName").getValue(String::class.java)
-                    val membersSnapshot = groupSnapshot.child("member")
-
-                    // 현재 사용자가 그룹 멤버에 포함되어 있는지 확인하는 변수
-                    var isMember = false
-
-                    // 각 멤버의 값을 확인하여 현재 사용자의 UID가 있는지 확인
-                    for (memberSnapshot in membersSnapshot.children) {
-                        val memberUid = memberSnapshot.getValue(String::class.java)
-                        if (memberUid == auth.currentUser?.uid) {
-                            isMember = true
-                            break
-                        }
-                    }
-
-                    // 그룹 이름이 일치하고, 현재 사용자가 그룹 멤버에 포함되어 있을 때
-                    if (groupName == targetGroupName && isMember) {
-                        groupId = groupSnapshot.key.toString()
-                        Log.d("kkk", groupId!!)
-
-                        // 메시지 가져오기 설정
-                        setupMessageListener(messageAdapter)
-                        break
-                    }
-                }
-            } else {
-                Log.d("ksh", "그룹이 존재하지 않습니다.")
-            }
-        }.addOnFailureListener {
-            Log.e("ksh", "그룹을 찾는 데 실패했습니다.")
-        }
-
+        /*
         // 메시지 전송 버튼 이벤트
         binding.btnSend.setOnClickListener {
             val message = binding.etMessage.text.toString()
