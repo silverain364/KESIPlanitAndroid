7fe932888af6439e5f37895f58b92f9a64d29722
kimsihyun <rlatlgus0129@naver.com>
2025-01-23 00:54:59 +0900

    fix. 채팅 기능 수정 완료, 그룹채팅방 리스트 조회 완료

--- app/src/main/java/com/example/kesi/fragment/GroupListFragment.kt
+++ app/src/main/java/com/example/kesi/fragment/GroupListFragment.kt
@@ -4,25 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.kesi.R
-import com.example.kesi.adapter.FriendListAdapter
 import com.example.kesi.adapter.GroupListAdapter
-import com.example.kesi.data.Group
-import com.example.kesi.data.User
-import com.example.kesi.databinding.FragmentFriendListBinding
+import com.example.kesi.api.GroupApi
 import com.example.kesi.databinding.FragmentGroupListBinding
+import com.example.kesi.model.GroupSimpleDto
+import com.example.kesi.setting.RetrofitSetting
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class GroupListFragment : Fragment() {
     lateinit var binding: FragmentGroupListBinding
     private lateinit var auth:FirebaseAuth
     private lateinit var database:DatabaseReference
+
+    private val retrofit = RetrofitSetting.getRetrofit();
+    private val groupApi = retrofit.create(GroupApi::class.java)
+
+    val groupList = ArrayList<GroupSimpleDto>()
+    val adapter = GroupListAdapter(groupList)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,41 +47,25 @@ class GroupListFragment : Fragment() {
         //db 초기화
         database = Firebase.database.reference
 
-        val groupList = ArrayList<Group>()
-        val adapter = GroupListAdapter(groupList)
-
         binding.recyclerView.layoutManager = LinearLayoutManager(requireActivity())
         binding.recyclerView.adapter = adapter
         binding.recyclerView.addItemDecoration(DividerItemDecoration(requireActivity(), LinearLayoutManager.VERTICAL))
 
-        // 그룹 데이터를 가져옴
-        database.child("groups").get().addOnSuccessListener { snapshot ->
-            if (snapshot.exists()) {
-                for (groupSnapshot in snapshot.children) {
-                    // 각 그룹의 멤버 UID 리스트를 가져옴
-                    val members = ArrayList<String>()
-                    for (memberSnapshot in groupSnapshot.child("member").children) {
-                        val memberUid = memberSnapshot.getValue(String::class.java)
-                        if (memberUid != null) {
-                            members.add(memberUid)
-                        }
-                    }
+        // 그룹 리스트 가져오기
+        getGroupList()
+    }
 
-                    // 현재 유저의 UID가 그룹의 멤버 목록에 포함되어 있는지 확인
-                    if (auth.currentUser?.uid in members) {
-                        val groupName = groupSnapshot.child("groupName").getValue(String::class.java) ?: "이름 없음"
-
-                        // 그룹을 리스트에 추가
-                        groupList.add(Group(groupName, members))
-                    }
-                }
-
-                // 어댑터에 데이터 변경 사항 알림
+    private fun getGroupList() {
+        groupApi.getAllGroups().enqueue(object : Callback<List<GroupSimpleDto>> {
+            override fun onResponse(p0: Call<List<GroupSimpleDto>>, response: Response<List<GroupSimpleDto>>) {
+                groupList.clear()
+                response.body()?.let { groupList.addAll(it) }
                 adapter.notifyDataSetChanged()
             }
-        }.addOnFailureListener {
-            // 데이터를 가져오는 중에 오류 발생 시 처리
-            it.printStackTrace()
-        }
+
+            override fun onFailure(p0: Call<List<GroupSimpleDto>>, p1: Throwable) {
+                Toast.makeText(context,"통신 실패", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
