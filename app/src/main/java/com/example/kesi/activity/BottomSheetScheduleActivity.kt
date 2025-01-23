a2e65a70e23dbff01e1fd1e4bc2d29f8f07b3e4f
silverain364 <soneunbi03@gmail.com>
2025-01-23 18:45:53 +0900

    BottomSheet 추가

---
+++ app/src/main/java/com/example/kesi/activity/BottomSheetScheduleActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kesi.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kesi.R
+import com.example.kesi.adapter.BottomSheetAdapter
+import com.example.kesi.model.BottomSheetScheduleDto
+
+class BottomSheetScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/kesi/model/BottomSheetScheduleDto.kt
@@ -0,0 +1,7 @@
+package com.example.kesi.model
+
+data class BottomSheetScheduleDto (
+    val iconResId: Int,
+    val title: String,
+    val time: String
+)
